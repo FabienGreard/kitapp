9f2204ef1e12a96c866cb675e67e60a157c651a6
Fabien <grd.fabien@gmail.com>
2017-12-02 21:11:20 +0100

    table init

--- src/modules/Admin/Users.js
+++ src/modules/Admin/Users.js
@@ -3,13 +3,14 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 import { userActions } from '../../_actions';
-import { Dialog } from '../../_components';
+import { Loading, Dialog } from '../../_components';
 import { theme } from '../../_helpers';
 
 //Material-ui import
-import { withStyles } from 'material-ui/styles';
 import Paper from 'material-ui/Paper';
 import Grid from 'material-ui/Grid';
+import { withStyles } from 'material-ui/styles';
+import Table, { TableBody, TableCell, TableHead, TableRow } from 'material-ui/Table';
 
 //styles
 const styles = context => ({
@@ -22,33 +23,92 @@ const styles = context => ({
     },
     paperContent: {
       padding: 16,
-      minHeight: 200,
+      overflow: 'auto',
+    },
+    table: {
+      minWidth: 700,
     },
 });
+
+
+  const getUserInfo = (user) => {
+    return {
+      id: user._id,
+      firstName: user.profile.firstName,
+      lastName: user.profile.lastName,
+      email: user.email,
+      role: user.role
+    }
+  };
 
 class UsersAdmin extends Component {
   constructor(props) {
     super(props);
     this.props.dispatch(userActions.getAll());
   }
+
   render() {
-    let { user, classes } = this.props;
+    let { users, classes, loading } = this.props;
+
     return (
-      <div>I'm an Admin page about users.</div>
+      <div>
+        { loading &&
+          <Loading mode="query"/>
+        }
+        <div className={classes.root}>
+          <Grid container spacing={24} className={classes.container}>
+            <Grid item xs={12} className={classes.item}>
+              <Paper className={classes.paper}>
+                <Dialog className={classes.title} message="Utilisateurs" style={theme.getRowStyle('darkGrey', 'none')} type="title"/>
+              </Paper>
+            </Grid>
+            <Grid item xs className={classes.item}>
+              <Paper className={classes.paperContent}>
+                <Table className={classes.table}>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Email</TableCell>
+                    <TableCell >Prénom</TableCell>
+                    <TableCell >Nom</TableCell>
+                    <TableCell >Role</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {Object.keys(users).length !== 0 &&
+                    users.map(
+                      (user) => (
+                        <TableRow key={getUserInfo(user).id}>
+                          <TableCell>{getUserInfo(user).email}</TableCell>
+                          <TableCell>{getUserInfo(user).firstName}</TableCell>
+                          <TableCell>{getUserInfo(user).lastName}</TableCell>
+                          <TableCell>{getUserInfo(user).role}</TableCell>
+                        </TableRow>
+                      )
+                    )
+                  }
+                </TableBody>
+                </Table>
+              </Paper>
+            </Grid>
+          </Grid>
+        </div>
+      </div>
     );
   }
 
 }
 
 UsersAdmin.propTypes = {
-  user: PropTypes.object.isRequired,
+  users: PropTypes.array,
   classes: PropTypes.object.isRequired,
 }
 
 function mapStateToProps(state) {
-    const { user } = typeof state.authentication.user !== 'undefined' ? state.authentication.user : { user: {} };
+  const { loading } = state.users
+    const { users } = typeof state.users.items !== 'undefined' ? state.users.items : { users: [] };
     return {
-        user
+        users,
+        loading
     };
 }
 
