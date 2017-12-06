c930a8c59c76eeaa6d25bc83f1d7ab1b3f8f2bd5
Fabien <grd.fabien@gmail.com>
2017-12-06 09:40:08 +0100

    change alert output Dialog => snackbar

--- src/_components/Alert.js
+++ src/_components/Alert.js
@@ -1,33 +1,36 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-//import { theme } from '../_helpers';
-
 //material-ui import
-//import Button from 'material-ui/Button';
-import Dialog, { /*DialogActions,*/ DialogContent, DialogContentText, /*DialogTitle*/ } from 'material-ui/Dialog';
 import Slide from 'material-ui/transitions/Slide';
 import { withStyles } from 'material-ui/styles';
+import Snackbar from 'material-ui/Snackbar';
+import ErrorOutlineIcon from 'material-ui-icons/ErrorOutline';
+
+//up for dialog && left for snackar
+const Transition = ({...props}) => (
+  <Slide direction="left" {...props} />
+);
 
 //styles
 const styles = context => ({
-  success: {
-    color: '#E91E63',
-  },
-  danger: {
-    color: '#FF9800',
-  },
+  message: {
+      display: 'flex',
+      alignItems: 'center',
+    },
+    icon: {
+      marginRight: context.spacing.unit,
+    },
 });
-
-const Transition = ({...props}) => (
-  <Slide direction="up" {...props} />
-);
 
 //Render an alert on actions error
 class Alert extends Component {
   constructor(props) {
     super(props);
-    this.state = { open: false };
+    this.state = {
+      open: false,
+      transition: Transition
+    };
   }
 
   componentWillReceiveProps(nextProps) {
@@ -46,23 +49,23 @@ class Alert extends Component {
 
   render() {
     let { alert, classes } = this.props;
+
+    let vertical = "bottom";
+    let horizontal = "right";
     return (
       <div>
-        <Dialog open={this.state.open} transition={Transition} keepMounted onRequestClose={this.handleRequestClose}>
-          <DialogContent>
-            <DialogContentText className={alert.type === 'alert-success' ? classes.success : classes.danger}>
-              {alert.message}
-            </DialogContentText>
-          </DialogContent>
-          {/*<DialogActions>
-            <Button onClick={this.handleRequestClose} color="primary">
-              Disagree
-            </Button>
-            <Button onClick={this.handleRequestClose} color="primary">
-              Agree
-            </Button>
-          </DialogActions>*/}
-        </Dialog>
+        <Snackbar
+          open={this.state.open}
+          anchorOrigin={{vertical, horizontal}}
+          onRequestClose={this.handleRequestClose}
+          transition={this.state.transition}
+          SnackbarContentProps={{
+            'aria-describedby': 'message-id',
+          }}
+          message={
+            <span id="message-id" className={classes.message}><ErrorOutlineIcon className={classes.icon}/>{alert.message}</span>
+          }
+        />
       </div>
     );
   }
